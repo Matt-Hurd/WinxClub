15f4052cd41d5e9f1c6ac2702e0156d68e5481cb
Matt-Hurd <matt-hurd@users.noreply.github.com>
2024-04-07 20:52:55 -0500

    Include ripped data, split out archive, include more data

---
+++ data/last_data.s
@@ -0,0 +1,45 @@
+	AREA data, DATA
+    GLOBAL gUnknown_08055AC0
+    GLOBAL gArchive
+
+gUnknown_08055AC0
+    INCBIN data/ripped/00055AC0.bin
+
+gArchive ; 00055CB8
+    INCBIN data/archive/header
+    INCBIN data/archive/sprites
+    INCBIN data/archive/sfx.bin
+    INCBIN data/archive/video_winxintro
+    INCBIN data/archive/xmusic.bin
+    INCBIN data/archive/mapblackswamp.mmstr
+    INCBIN data/archive/mapcloudtower.mmstr
+    INCBIN data/archive/redfountain.mmstr
+    INCBIN data/archive/video_fireescape
+    INCBIN data/archive/video_trixarmy
+    INCBIN data/archive/gardenia.mmstr
+    INCBIN data/archive/video_diaspro
+    INCBIN data/archive/video_finale
+    INCBIN data/archive/mapalfea.mmstr
+    INCBIN data/archive/txtpackage_all.bin
+    INCBIN data/archive/video_babyfire
+    INCBIN data/archive/compresspic.mmstr
+    INCBIN data/archive/domino.mmstr
+    INCBIN data/archive/video_stellawins
+    INCBIN data/archive/scriptdata.bin
+    INCBIN data/archive/video_vinetrapped
+    INCBIN data/archive/video_bloomtransform
+    INCBIN data/archive/bgmenupuzzle.bin
+    INCBIN data/archive/bgmenupuzzlepic.bin
+    INCBIN data/archive/fontpondoresa12.fnt
+    INCBIN data/archive/bgmenusave
+    INCBIN data/archive/fontgothic12.fnt
+    INCBIN data/archive/bgmenuwarp
+    INCBIN data/archive/bgmenuinvent
+    INCBIN data/archive/bgmenufavours
+    INCBIN data/archive/bgmenuobjectives
+    INCBIN data/archive/bgmenumagic
+    INCBIN data/archive/bgmenuoptions
+    INCBIN data/archive/bgmenuscrapbook
+    INCBIN data/archive/mapexemple.mmstr
+
+    END
